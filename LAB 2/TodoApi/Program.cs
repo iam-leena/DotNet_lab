ce6e74f6cf197c4550c93808d8d1c12d7e7e434a
iam-leena <sabita.shrestha19@apexcollege.edu.np>
2024-01-29 10:29:38 +0545

    start lab 3

--- LAB 2/TodoApi/Program.cs
+++ LAB 2/TodoApi/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<TodoContext>(opt =>
 opt.UseInMemoryDatabase("TodoList"));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
